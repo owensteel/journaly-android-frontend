dee37ec9d0bb60662bc8459f7afd8cec56f75cc3
Owen Steel <owensteel@google.com>
2024-07-28 14:58:08 +0000

    Sets up Java activity test

---
+++ app/src/main/java/com/owensteel/journaly/MainActivity.java
@@ -0,0 +1,15 @@
+package com.owensteel.journaly;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    // Initialize UI components and set up event listeners here
+  }
+}
